7dd7dcd7c3f5e265fce843d57adb5baa76214a7e
Sajjad <pb.sajjad@gmail.com>
2022-08-21 19:34:00 +0430

    Handle CORS

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method == "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
 app.get("/api/settings", (req, res) => {
   const lang = req.query.lang || "en";
 
